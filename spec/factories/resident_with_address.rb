d081c04dea9088485165e027a15c4d49d538e5ad
ewertoncodes <ewertonorg@gmail.com>
2023-05-11 03:58:13 -0300

    list residents with address

---
+++ spec/factories/resident_with_address.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+  factory :resident_with_address, class: "Resident" do
+    full_name { "John Doe" }
+    cpf { Faker::IDNumber.brazilian_citizen_number }
+    cns { "123456789012345" }
+    email { "johndoe@example.com" }
+    birth_date { "1990-01-01" }
+    phone { "11999999999" }
+    photo { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'image.jpeg'), 'image/jpeg') }
+    status { "active" }
+    address_attributes {
+      {
+        zip_code: "12345678",
+        street: "Example Street",
+        neighborhood: "Example Neighborhood",
+        city: "Example City",
+        state: "ES",
+        complement: "Example complement"
+      }
+    }
+  end
+end
---
+++ spec/features/resident_index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Resident Index', type: :feature do
+  scenario 'displays a table with residents and their addresses' do
+    resident = create(:resident_with_address)
+    visit residents_path
+
+    expect(page).to have_css('h1', text: 'Residentes')
+
+    within('table') do
+      expect(page).to have_content(resident.full_name)
+      expect(page).to have_content(resident.cpf)
+      expect(page).to have_content(resident.cns)
+      expect(page).to have_content(resident.email)
+      expect(page).to have_content(resident.birth_date.strftime('%d/%m/%Y'))
+      expect(page).to have_content(resident.phone)
+      expect(page).to have_content(resident.address.zip_code)
+      expect(page).to have_content(resident.address.street)
+      expect(page).to have_content(resident.address.neighborhood)
+      expect(page).to have_content(resident.address.city)
+      expect(page).to have_content(resident.address.state)
+      expect(page).to have_content(resident.address.complement)
+    end
+  end
+end
