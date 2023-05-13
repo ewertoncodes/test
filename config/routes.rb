b1d2aeb0658b342c5fbc8e76a6f02fbf21dc5d8d
ewertoncodes <ewertonorg@gmail.com>
2023-05-13 16:45:22 -0300

    form as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'residents#new'
   resources :residents
-  resources :address
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
