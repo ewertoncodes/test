70776258b73fe824c1f3514266697a4b87dac7f4
ewertoncodes <ewertonorg@gmail.com>
2023-05-13 22:20:41 -0300

    remove fly.io

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module Ewerton
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << "municipe-test.fly.dev"
   end
 end
